8efb4888172e3e164f9fd1da904683edb76368bd
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2022-01-23 22:12:19 +0300

    added future builder

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:weather_app/WeatherModel.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
+String url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Kızıltepe&appid=3bd5ba9c630e384ffbf8de1f536042fe";
+
+    WeatherModel weather = WeatherModel();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -31,21 +38,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String? responseWeather;
 
   Future<void> _incrementCounter() async {
-    String url =
-        "https://api.openweathermap.org/data/2.5/weather?q=Kızıltepe&appid=3bd5ba9c630e384ffbf8de1f536042fe";
-
     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
+      
       Map responseJSON = jsonDecode(response.body);
-      responseWeather = responseJSON["name"].toString()+  " " + responseJSON["sys"]["country"].toString();
+
+      weather.name = responseJSON['name'].toString();   
+      weather.weather = responseJSON['weather']['main'].toString();   
+      weather.temperature = responseJSON['main']['temp'].toString();   
     } else {
-      responseWeather = "Baglantida sorun olustu!!";
+      "Baglantida sorun olustu!!";
     }
     setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //_incrementCounter();
   }
 
   @override
@@ -54,20 +67,28 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: <Widget>[
-          Text(
-            'GELEN CEVAP',
-            style: Theme.of(context).textTheme.headline3,
-          ),
-          Text(responseWeather == null ? "Deger yok" : "$responseWeather",
-              style: TextStyle(fontSize: 36))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'GELEN CEVAP',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            FutureBuilder(
+              future: http.get(Uri.parse(url)),
+              builder: (context, snapshot){
+                if (snapshot.hasData) {
+                  _incrementCounter();
+                  return Text(weather.weather.toString() , style: TextStyle(fontSize: 24),);
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
